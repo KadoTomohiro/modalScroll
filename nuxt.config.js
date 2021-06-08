dc1e4b028cfd71dacbdc1f305b682d6aa42a4635
KadoTomohiro <>
2021-06-08 15:45:52 +0900

    base href setting

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,15 +11,16 @@ export default {
     htmlAttrs: {
       lang: 'en',
     },
-    metaInfo: {
-      base: { href: '/modalScroll/' },
-    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  router: {
+    base: '/modalScroll/',
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
